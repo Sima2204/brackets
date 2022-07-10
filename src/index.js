759b73c333b9995ea8a6588577552ea0df32369d
Sima2204 <vlad.simuta@gmail.com>
2022-07-10 23:29:14 +0300

    fix: some part done, but not all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-  not easy
+function check(str, bracketsConfig) {
+
+  if (str.length % 2 != 0) {
+      return false;
+  };
+  
+  const brackets = {
+      ")": "(",
+      "]": "[",
+      "}": "{"
+  };
+  
+  const stack = [];
+    for (let i = 0; i < str.length; i++) {
+          if (isClosedBracket(str[i])) {
+              if (brackets[str[i]] !== stack.pop()) 
+              return false;
+          } else {
+              stack.push(str[i]);
+          }
+    }
+  return stack.length === 0;
+}
+  
+function isClosedBracket(ch) {
+  return [")", "]", "}"].indexOf(ch) > -1;
 }
