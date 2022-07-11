389c6ad203f4948659de871762608aa00ac3c946
Sima2204 <vlad.simuta@gmail.com>
2022-07-11 23:11:36 +0300

    fix: not all tests passed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
+  
+  let OPENED_BRACKETS = [];
+  let CLOSED_BRACKETS = [];
+  let SAME_BRACKETS = [];
 
-  if (str.length % 2 != 0) {
-      return false;
+  for (let item of bracketsConfig) {
+
+    if (item[0] === item[1]) {
+        SAME_BRACKETS.push(item[0]);
+    } else {
+        CLOSED_BRACKETS.push(item[1]);
+        OPENED_BRACKETS.push(item[0]);
+    };
   };
-  
-  const brackets = {
-      ")": "(",
-      "]": "[",
-      "}": "{"
-  };
-  
-  const stack = [];
-    for (let i = 0; i < str.length; i++) {
-          if (isClosedBracket(str[i])) {
-              if (brackets[str[i]] !== stack.pop()) 
-              return false;
-          } else {
-              stack.push(str[i]);
-          }
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+
+    let currentSymbol = str[i];
+    let topStackElement = stack[stack.length - 1];
+
+    if (OPENED_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else if (SAME_BRACKETS.includes(currentSymbol)) {
+      if (currentSymbol === topStackElement) {
+        stack.pop();
+      } else {
+        stack.push(currentSymbol);
+      }
+    } else {
+      if (OPENED_BRACKETS[CLOSED_BRACKETS.indexOf(currentSymbol)] === topStackElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
     }
+  }
   return stack.length === 0;
-}
-  
-function isClosedBracket(ch) {
-  return [")", "]", "}"].indexOf(ch) > -1;
 }
